25ce6ff5f88f0563738c0634d8ab452be84bd67d
Bruno <bruno@bruno.com>
2017-12-21 20:44:21 -0200

    Adicionando preço

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -1,5 +1,6 @@
 package br.com.caelum.ingresso.model;
 
+import java.math.BigDecimal;
 import java.time.LocalTime;
 
 import javax.persistence.Entity;
@@ -10,48 +11,68 @@ import javax.persistence.ManyToOne;
 @Entity
 public class Sessao {
 
+	private BigDecimal preco;
 	@Id
 	@GeneratedValue
 	private Integer id;
 	private LocalTime horario;
-	
+
 	@ManyToOne
 	private Sala sala;
-	
+
 	@ManyToOne
 	private Filme filme;
-	
-	public Sessao(){
-		
+
+	public Sessao() {
+
 	}
-	public Sessao(LocalTime horario, Filme filme, Sala sala){
+
+	public Sessao(LocalTime horario, Filme filme, Sala sala) {
 		this.horario = horario;
 		this.filme = filme;
 		this.sala = sala;
+		this.preco = sala.getPreco().add(filme.getPreco());
 	}
+
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public LocalTime getHorario() {
 		return horario;
 	}
+
 	public void setHorario(LocalTime horario) {
 		this.horario = horario;
 	}
+
 	public Sala getSala() {
 		return sala;
 	}
+
 	public void setSala(Sala sala) {
 		this.sala = sala;
 	}
+
 	public Filme getFilme() {
 		return filme;
 	}
+
 	public void setFilme(Filme filme) {
 		this.filme = filme;
 	}
-	
+
 }
